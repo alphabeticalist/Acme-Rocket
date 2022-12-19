ae6aec15e42520414f4ec6a43def43f18492a119
alphabeticalist <alphbeticalist@gmail.com>
2022-12-19 11:31:00 +0100

    latest commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/*.html", "./dist/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreens: { raw: "(min-aspect-ratio: 3/2)" },
+        fullscreen: { raw: "(max-aspect-ratio: 13/20)" },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
